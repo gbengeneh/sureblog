a77214e11db8ea78ae5a96c6f83a940a47d8dfbd
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2025-10-13 13:47:36 +0100

    update create post

--- src/components/PostCreate.jsx
+++ src/components/PostCreate.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { getCategories } from '../../api';
+import { getCategories, createPost } from '../../api';
 
 const PostCreate = () => {
   const [formData, setFormData]= useState({title: '', content: '', category_id: '', image: null});
@@ -23,7 +23,7 @@ const PostCreate = () => {
 
   const handleChange = (e) =>{
     if(e.target.name =='image'){
-      setFormData({ ...formData, image: e.terget.files[0]});
+      setFormData({ ...formData, image: e.target.files[0]});
     }else{
       setFormData({ ...formData, [e.target.name]: e.target.value})
     }
@@ -54,9 +54,23 @@ const PostCreate = () => {
   return (
     <div className='post-form-container'>
       <h2>Create Post</h2>
-      
+      <form className="post-form" encType="multipart/form-data" onSubmit={handleSubmit}>
+        <input type="text" name='title' placeholder='Enter Post title' value={formData.title} onChange={handleChange} required />
+
+        <textarea name="content" id="content" value={formData.content} onChange={handleChange} placeholder='Content' required></textarea>
+
+        <select name="category_id" value={formData.category_id} onChange={handleChange} required>
+          <option value="">Select Category</option>
+          {categories.map(cat =>(
+            <option key={cat.id} value={cat.id}>{cat.name}</option>
+          ))}
+        </select>
+        <input type="file" name='image' accept='image/*' onChange={handleChange} />
+        <button type='submit'>Create Post</button>
+      </form>
+      {error && <p className='error'>{error}</p>}
     </div>
-  )
-}
+  );
+};
 
 export default PostCreate
